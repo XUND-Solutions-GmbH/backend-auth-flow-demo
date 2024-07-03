08e179bbca6cea93f3081300616f4b9dca4a827a
Márton Varga <varga@xund.ai>
2024-07-03 17:24:08 +0200

    refactor

--- src/index.ts
+++ src/index.ts
@@ -1,56 +1,41 @@
-import express, { Request, Response } from 'express';
-import dotenv from 'dotenv';
-import { getApiBaseUrl, getApiKey, getAuthBaseUrl, getClientId, getRedirectUri, getTokenType } from './variables';
-import { authorize, waitForAuthCode, getJWTToken, sendXUNDApiRequest } from './functions';
+import express from "express";
+import dotenv from "dotenv";
+import axios from "axios";
 
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 8000;
 
-let authCode: string | undefined = undefined;
-let jwtAccessToken: string | undefined = undefined;
-
-app.get('/redirect', async (req: Request, res: Response) => {
-  authCode = req.query.code as string;
-  res.sendStatus(200);
+app.get("/redirect", async (req, res) => {
+  res.end()
 });
 
-app.get('/', async (req: Request, res: Response) => {
-  // Step 1: Call /authorize endpoint 
-  await authorize({ baseUrl: getAuthBaseUrl(), clientId: getClientId(), redirectUri: getRedirectUri() });
+app.get("/", async (req, res) => {
+  // Step 1: Call /authorize endpoint
+  const authorizeURL = `${process.env.XUND_AUTH_BASE_URL}/authorize?clientId=${process.env.XUND_AUTH_CLIENT_ID}&redirectUri=${process.env.XUND_AUTH_REDIRECT_URI}`;
+  const authorizeResponse = await axios.get<{ authCode: string }>(authorizeURL);
+  const authCode = authorizeResponse.request.path.match(/code=([^&]+)/)[1];
 
-  // Step 2: Wait for receiving the auth code via /redirect endpoint
-  try {
-    await waitForAuthCode(() => authCode);
-    if (!authCode) {
-      throw Error('Undefined auth code');
-    }
-  } catch (error) {
-    res.json({ message: 'Failed to get an auth code', error });
-    return;
-  }
-
-  // Step 3: Get JWT access token
-  jwtAccessToken = await getJWTToken({
-    baseUrl: getAuthBaseUrl(),
-    clientId: getClientId(),
-    authCode: authCode,
-    apiKey: getApiKey(),
+    // Step 2: Get JWT access token
+  const tokenURL = `${process.env.XUND_AUTH_BASE_URL}/token?clientId=${process.env.XUND_AUTH_CLIENT_ID}&authCode=${authCode}`;
+  const tokenResponse = await axios.get<{ accessToken: string }>(tokenURL, {
+    headers: { "api-key": process.env.XUND_AUTH_API_KEY },
   });
 
-  if (!jwtAccessToken) {
-    res.json({ message: 'Failed to get a jwt access token' });
-    return;
-  }
-
-  // Step 4: Use XUND API
-  const xundApiRes = await sendXUNDApiRequest({ apiBaseUrl: getApiBaseUrl(), tokenType: getTokenType(), jwtAccessToken: jwtAccessToken });
+  // Step 3: Use XUND API
+  const xundApiURL = `${process.env.XUND_API_BASE_URL}/v1/imprintResources`;
+  const xundApiRes = await axios.get(xundApiURL, {
+    headers: {
+      authorization: `Bearer ${tokenResponse.data.accessToken}`,
+      language: "en",
+    },
+  });
 
   const response = {
-    access_token: jwtAccessToken,
-    token_type: 'Bearer',
-    xund_api_response: xundApiRes,
+    access_token: tokenResponse.data.accessToken,
+    token_type: "Bearer",
+    xund_api_response: xundApiRes.data,
   };
   res.send(response);
 });
