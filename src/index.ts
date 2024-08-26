820b9c78081233da3ecaa8c6b3819f735e21c940
Marton Szabo <szabo@xund.ai>
2024-08-26 12:26:52 +0200

    fix: latest auth changes

--- src/index.ts
+++ src/index.ts
@@ -15,6 +15,9 @@ app.get("/redirect", async (req, res) => {
 });
 
 app.get("/", async (req, res) => {
+  const FRONTEND_AUTH = process.env.FRONTEND_AUTH == "true";
+  let response:any = {};
+  if (FRONTEND_AUTH) {
   // Step 1: Call /authorize endpoint
   let authorizeURL_authCode = `${process.env.XUND_AUTH_BASE_URL}/authorize?clientId=${process.env.XUND_AUTH_CLIENT_ID}&scope=state&state=${STATE}`;
   if (process.env.AUTH_CODE) {
@@ -33,12 +36,16 @@ app.get("/", async (req, res) => {
 
     // Step 2: Get JWT access token
   const tokenURL_authCode = `${process.env.XUND_AUTH_BASE_URL}/token?clientId=${process.env.XUND_AUTH_CLIENT_ID}&authCode=${authCode}&redirectUri=${process.env.XUND_AUTH_REDIRECT_URI}`;
-  const tokenResponse_authCode = await axios.get<{ accessToken: string }>(tokenURL_authCode );
+  const tokenResponse_authCode = await axios.get<{ accessToken: string }>(tokenURL_authCode, {
+    headers: {
+    }
+  });
+
+  console.log(`authCode token status: ${tokenResponse_authCode.status}`);
 
   const token = tokenResponse_authCode.request.res.responseUrl.match(/\/#([^&]+)/)[1];
 
   if (token) {
-    console.log(`authCode token status: ${tokenResponse_authCode.status}`);
     console.log(`authCode token data: ${token}`);
   }
 
@@ -67,6 +74,7 @@ app.get("/", async (req, res) => {
     console.log(`noState authorize status: ${authorizeResponse.status}`);
     console.log(`noState authorize res data: ${JSON.stringify(authorizeResponse.data,null,2)}`);
     authCode_noState = authorizeResponse_authCode.data.authCode;
+    //state_authCode_noState = authorizeResponse_authCode.data.state;
     console.log(authCode_noState);
   }
   catch (err) {
@@ -88,11 +96,25 @@ app.get("/", async (req, res) => {
     console.log(`noState token data: ${token_noState}`);
   }
 
+    response.authCode = {
+      accessToken: token,
+      state: state_authCode,
+      token_type: "Bearer",
+      xund_api_response: xundApiRes_authCode?.data,
+    };
+    response.noState = {
+      accessToken: token_noState,
+      state: state_authCode,
+      token_type: "Bearer",
+    };
+  }
+
     // Get JWT access token with POST clientId and clientSecret (we use the api-key as clientSecret)
-  const tokenURL_POST = `${process.env.XUND_AUTH_BASE_URL}/token?grant_type=client_credentials&scope=state&state=${STATE}`;
+  const tokenURL_POST = `${process.env.XUND_AUTH_BASE_URL}/token?grant_type=client_credentials&state=${STATE}`;
   const tokenResponse_POST = await axios.post<{ access_token: string, token_type: string }>(tokenURL_POST, {
     clientId: process.env.XUND_AUTH_CLIENT_ID,
     clientSecret: process.env.XUND_AUTH_API_KEY,
+    grant_type: 'client_credentials',
   });
 
     console.log(`clientSecret token status: ${tokenResponse_POST.status}`);
@@ -136,22 +158,14 @@ app.get("/", async (req, res) => {
     }
   }
 
-  const response = {
-    clientCredentials_POST: {
+  response.clientCredentials_POST = {
       accessToken: tokenResponse_POST.data.access_token,
       token_type: "Bearer",
       xund_api_response: xundApiRes_POST?.data,
-    },
-    apiKey: {
+    };
+    response.apiKey = {
       xund_api_response: xundApiRes_apiKey?.data,
-    },
-    authCode: {
-      accessToken: token,
-      state: state_authCode,
-      token_type: "Bearer",
-      xund_api_response: xundApiRes_authCode?.data,
-    },
-  };
+    };
   res.send(response);
 });
 
